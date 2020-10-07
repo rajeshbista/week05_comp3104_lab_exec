1f232bc2017abf2f6b0f382d43c88007e3f99441
Rajesh Bista <rajesh.bista@georgebrown.ca>
2020-10-07 14:31:03 -0400

    added all files

---
+++ build/precache-manifest.a45884128617fb2761f56f01e2e95bde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e60f6d7ca0b5e634c189",
+    "url": "/static/css/main.787dd286.chunk.css"
+  },
+  {
+    "revision": "e60f6d7ca0b5e634c189",
+    "url": "/static/js/main.e60f6d7c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "a1758dc6edcb11c526f588fba012a2e5",
+    "url": "/index.html"
+  }
+];
